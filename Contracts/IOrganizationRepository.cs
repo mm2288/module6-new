56ccaff563cf4a0665d03198c45de6b21e5965cb
mm2288 <mm2288@njit.edu>
2021-04-19 17:08:28 -0400

    added code

--- Contracts/IOrganizationRepository.cs
+++ Contracts/IOrganizationRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace Contracts
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Contracts
 {
     public interface IOrganizationRepository
     {
+        IEnumerable<Organization> GetAllOrganizations(bool trackChanges);
+        Organization GetOrganization(Guid companyId, bool trackChanges);
     }
 }
